7740c2fb40f4aaf14eac878b907458c848c933c8
Daniel J Blueman <daniel@quora.org>
2024-10-13 09:53:09 +0800

    Add validation of weekday

--- main.go
+++ main.go
@@ -11,9 +11,18 @@ import (
 var (
    ErrMalformed = errors.New("In: malformed timespec")
    specRe = regexp.MustCompile(`(?:(\w{3})(?:-(\w{3}))? )?(\d{2})-(\d{2})`)
+   dayMap = map[string]time.Weekday{
+      "Sun": time.Sunday,
+      "Mon": time.Monday,
+      "Tue": time.Tuesday,
+      "Wed": time.Wednesday,
+      "Thu": time.Thursday,
+      "Fri": time.Friday,
+      "Sat": time.Saturday,
+   }
 )
 
-func In(time time.Time, spec string, timezone *time.Location) (bool, error) {
+func In(t time.Time, spec string, timezone *time.Location) (bool, error) {
    fmt.Printf("spec %s\n", spec)
    matches := specRe.FindAllStringSubmatch(spec, -1)
    if matches == nil {
@@ -31,19 +40,29 @@ func In(time time.Time, spec string, timezone *time.Location) (bool, error) {
          return false, ErrMalformed
       }
 
-      startDay := "Mon"
-      endDay   := "Sun"
+      startDayStr := "Mon"
+      endDayStr   := "Sun"
 
       switch len(match) {
       case 3:
-         startDay = match[1]
-         endDay   = startDay
+         startDayStr = match[1]
+         endDayStr   = startDayStr
       case 4:
-         startDay = match[1]
-         endDay   = match[2]
+         startDayStr = match[1]
+         endDayStr   = match[2]
       }
 
-      fmt.Printf("startDay=%s endDay=%s startHour=%02d endHour=%02d\n", startDay, endDay, startHour, endHour)
+      startDay, ok := dayMap[startDayStr]
+      if !ok {
+         return false, ErrMalformed
+      }
+
+      endDay, ok := dayMap[endDayStr]
+      if !ok {
+         return false, ErrMalformed
+      }
+
+      fmt.Printf("startDay=%d endDay=%d startHour=%02d endHour=%02d\n", startDay, endDay, startHour, endHour)
    }
 
    return false, nil
